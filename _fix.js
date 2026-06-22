c8b332e48363b1ea057937a1b1639abb3619556e
tegar <tegartanjung141@gmail.com>
2026-06-22 13:06:23 -0700

    auto

--- _fix.js
+++ _fix.js
@@ -1,1 +1,6 @@
-var fs=require('fs');var c=fs.readFileSync('web/app.jsx','utf8');console.log(c.length);  
+var fs=require('fs');  
+var c=fs.readFileSync('web/app.jsx','utf8');  
+// Bug #4: openCanvas A2UI_STREAMING  
+var o1=\"const state = p.flutter\n      ? { flutter: p.source, doc: FLUTTER_COMPILING, files: p.files }\n      : { doc: p.doc || FLUTTER_COMPILING, run };\"  
+var n1=\"const state = p.flutter\n      ? { flutter: p.source, doc: p.a2ui ? A2UI_STREAMING : FLUTTER_COMPILING, files: p.files }\n      : { doc: p.doc || FLUTTER_COMPILING, run };\"  
+c=c.replace(o1,n1);console.log('Bug4:',c.includes(o1));  
